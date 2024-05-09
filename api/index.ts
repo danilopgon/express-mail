eacc6282cc44c53c9c4e52537f91e04be700692d
danilopgon <dani.lopezgonzalez.94@gmail.com>
2024-05-09 19:53:01 +0200

    add some validations

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { check, validationResult } from 'express-validator';
 
 interface MailBody {
   name: string;
@@ -22,34 +23,53 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World from Vercel!');
 });
 
-app.post('/api/mail', (req: Request, res: Response) => {
-  const { name, email, phone, subject, message } = req.body as MailBody;
-
-  const transporter = nodemailer.createTransport({
-    host: 'uk5.fcomet.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.NODEMAILER_USER,
-      pass: process.env.NODEMAILER_PASSWORD,
-    },
-  });
-
-  const mailOptions = {
-    from: email,
-    to: process.env.MAIL_TO,
-    subject: subject,
-    text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
-  };
-
-  transporter.sendMail(mailOptions, (error: any, info: nodemailer.SentMessageInfo) => {
-    if (error) {
-      return res.status(500).send(error);
+app.post(
+  '/api/mail',
+  [
+    // validation rules
+    check('name').notEmpty().withMessage('El normbre es requerido'),
+    check('email').isEmail().withMessage('El email no es válido'),
+    check('phone').notEmpty().withMessage('El teléfono es requerido'),
+    check('subject').notEmpty().withMessage('El asunto es requerido'),
+    check('message').notEmpty().withMessage('El mensaje es requerido'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    res.status(200).send('Email sent: ' + info.response);
-  });
-});
+    const { name, email, phone, subject, message } = req.body as MailBody;
+
+    const transporter = nodemailer.createTransport({
+      host: 'uk5.fcomet.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.NODEMAILER_USER,
+        pass: process.env.NODEMAILER_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: email,
+      to: process.env.MAIL_TO,
+      subject: subject,
+      text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
+    };
+
+    transporter.sendMail(
+      mailOptions,
+      (error: any, info: nodemailer.SentMessageInfo) => {
+        if (error) {
+          return res.status(500).send(error);
+        }
+
+        res.status(200).send('Email sent: ' + info.response);
+      }
+    );
+  }
+);
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
 
